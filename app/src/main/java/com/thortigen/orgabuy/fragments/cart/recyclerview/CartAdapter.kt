a61aec6c50c675736bd4563e2a224c283ceae4f0
AMD\Serg <serg_mymail@mail.ru>
2021-08-13 23:18:23 +0300

    Add removing item from cart

--- app/src/main/java/com/thortigen/orgabuy/fragments/cart/recyclerview/CartAdapter.kt
+++ app/src/main/java/com/thortigen/orgabuy/fragments/cart/recyclerview/CartAdapter.kt
@@ -1,26 +1,36 @@
 package com.thortigen.orgabuy.fragments.cart.recyclerview
 
+import android.app.Application
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.thortigen.orgabuy.data.models.CartItem
+import com.thortigen.orgabuy.data.models.ShopListItem
 import com.thortigen.orgabuy.databinding.FragmentCartRvItemBinding
+import com.thortigen.orgabuy.viewmodels.CartViewModel
+import com.thortigen.orgabuy.viewmodels.ShopListViewModel
 
 class CartAdapter : RecyclerView.Adapter<CartAdapter.ItemViewHolder>() {
 
     var cartItemList = emptyList<CartItem>()
 
+    private val mCartViewModel = CartViewModel(application = Application())
+    private val mShopListViewModel = ShopListViewModel(application = Application())
+
     class ItemViewHolder(private val binding: FragmentCartRvItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-            fun bind(cartItem: CartItem) {
-                binding.cartItem = cartItem
-                binding.executePendingBindings()
-            }
+        val imageButton: ImageButton = binding.rvIbRemoveFromCart
+
+        fun bind(cartItem: CartItem) {
+            binding.cartItem = cartItem
+            binding.executePendingBindings()
+        }
 
         companion object {
-            fun from(parent: ViewGroup) : ItemViewHolder{
+            fun from(parent: ViewGroup): ItemViewHolder {
 
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = FragmentCartRvItemBinding.inflate(layoutInflater, parent, false)
@@ -36,6 +46,14 @@ class CartAdapter : RecyclerView.Adapter<CartAdapter.ItemViewHolder>() {
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val currentCartItem = cartItemList[position]
+
+        holder.imageButton.setOnClickListener {
+            mCartViewModel.deleteItem(currentCartItem)
+            val shopListItem = ShopListItem(currentCartItem.id, currentCartItem.name, currentCartItem.description, 0)
+            mShopListViewModel.editItem(shopListItem)
+        }
+
+
         holder.bind(currentCartItem)
     }
 
@@ -43,7 +61,7 @@ class CartAdapter : RecyclerView.Adapter<CartAdapter.ItemViewHolder>() {
         return cartItemList.size
     }
 
-    fun setData(cartItems : List<CartItem>) {
+    fun setData(cartItems: List<CartItem>) {
         val cartAdapterDiffUtils = CartAdapterDiffUtils(cartItemList, cartItems)
         val cartAdapterDiffResult = DiffUtil.calculateDiff(cartAdapterDiffUtils)
         cartItemList = cartItems
