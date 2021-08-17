d9f9e68cb76c017ddb22823cc6b8a09c547b42c9
AMD\Serg <serg_mymail@mail.ru>
2021-08-17 23:18:59 +0300

    Add sorting items in shopping list

--- app/src/main/java/com/thortigen/orgabuy/fragments/shoplist/ShopListFragment.kt
+++ app/src/main/java/com/thortigen/orgabuy/fragments/shoplist/ShopListFragment.kt
@@ -28,7 +28,7 @@ class ShopListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentShopListBinding.inflate(inflater, container, false)
 
@@ -41,7 +41,7 @@ class ShopListFragment : Fragment() {
             viewLifecycleOwner,
             {
                 data -> mShoplistViewModel.checkForShopListIsEmpty(data)
-                shopListAdapter.setData(data)
+                shopListAdapter.setData(data.sortedBy { it.name }.sortedBy { it.isInCart })
             }
         )
 
